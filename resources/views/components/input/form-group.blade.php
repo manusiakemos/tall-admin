ff0fe664180a3bb7c832089d534072c78d58366a
Hafiz <manusiakemos@gmail.com>
2022-02-07 20:12:01 +0800

    update label

--- resources/views/components/input/form-group.blade.php
+++ resources/views/components/input/form-group.blade.php
@@ -1,5 +1,5 @@
 <div class="my-3 pb-1">
-    <label for="{{$key}}" class="text-sm">{{$label}}</label>
+    <label for="{{$key}}" class="text-sm text-gray-700 dark:text-gray-300">{{$label}}</label>
     {{$slot}}
 
     @error($model)
